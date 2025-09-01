ec23e45da1f5c50b0934e42cbf4dec052775c9b9
Олег Сливканич <TERRI2000@users.noreply.github.com>
2025-09-01 19:30:46 +0300

    Fixed some logout bug pt3

--- controllers/authController.js
+++ controllers/authController.js
@@ -202,7 +202,6 @@ exports.logout = (req, res) => {
     path: '/',
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
   });
   
 
