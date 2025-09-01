4818fdfa414e0cfafc679f3efaf6e9512cdfec09
Олег Сливканич <TERRI2000@users.noreply.github.com>
2025-09-01 20:58:41 +0300

    Add some logs

--- controllers/authController.js
+++ controllers/authController.js
@@ -198,16 +198,10 @@ exports.isLoggedIn = catchAsync(async (req, res, next) => {
 
 exports.logout = (req, res) => {
   console.log('=== LOGOUT REQUEST ===');
-  console.log('Method:', req.method);
-  console.log('URL:', req.url);
-  console.log('Headers:', req.headers);
   console.log('Cookies before clear:', req.cookies);
-  console.log('Secure:', req.secure);
-  console.log('X-Forwarded-Proto:', req.headers['x-forwarded-proto']);
   
   // Очищуємо cookie з ТОЧНО тими ж опціями, що й при встановленні
   res.clearCookie('jwt', {
-    path: '/',
     httpOnly: true,
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
   });
